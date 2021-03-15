027aaaf29773e267ca0501553dd6591012a73677
Ulbricht, Marian <ulbricht@innoroute.de>
2021-03-15 10:33:45 +0100

    new das tag

--- net/dsa/tag_innoroute.h
+++ net/dsa/tag_innoroute.h
@@ -9,7 +9,7 @@
 #define INR_debug_RX 0
 #define INR_debug_TX 0
 #define DEBUG 1
-#define DSA_TAG_VERSION 3
+#define DSA_TAG_VERSION 4
 
 
 struct INR_TIME_timestamps
@@ -18,18 +18,7 @@ struct INR_TIME_timestamps
   uint64_t controlled;
 };
 
-struct INR_DSA_TAG_TX		//from CPU
-{
 
-  uint32_t DSA_ETH_TYPE:16;
-  uint32_t INGRESS_PORT:6;
-  uint32_t EGRESS_PORT:6;
-  uint32_t STREAM_Q:4;
-  uint32_t TX_TIMESTAMP;
-  uint32_t TX_CONFIRMATION_ID;
-  uint32_t reserved;
-
-} __attribute__((__packed__, scalar_storage_order ("big-endian")));
 
 #if DSA_TAG_VERSION == 1
 #define INR_TAG_LEN	16
@@ -42,6 +31,18 @@ struct INR_DSA_TAG_RX		// to cpu
   uint8_t PROC_RSN:3;
   uint8_t BAD_0:1;
   uint64_t RX_timestamp;
+  uint32_t reserved;
+
+} __attribute__((__packed__, scalar_storage_order ("big-endian")));
+struct INR_DSA_TAG_TX		//from CPU
+{
+
+  uint32_t DSA_ETH_TYPE:16;
+  uint32_t INGRESS_PORT:6;
+  uint32_t EGRESS_PORT:6;
+  uint32_t STREAM_Q:4;
+  uint32_t TX_TIMESTAMP;
+  uint32_t TX_CONFIRMATION_ID;
   uint32_t reserved;
 
 } __attribute__((__packed__, scalar_storage_order ("big-endian")));
@@ -61,6 +62,18 @@ struct INR_DSA_TAG_RX		// to cpu
   uint32_t TXF_CTRL_timestamp;
 
 } __attribute__((__packed__, scalar_storage_order ("big-endian")));
+struct INR_DSA_TAG_TX		//from CPU
+{
+
+  uint32_t DSA_ETH_TYPE:16;
+  uint32_t INGRESS_PORT:6;
+  uint32_t EGRESS_PORT:6;
+  uint32_t STREAM_Q:4;
+  uint32_t TX_TIMESTAMP;
+  uint32_t TX_CONFIRMATION_ID;
+  uint32_t reserved;
+
+} __attribute__((__packed__, scalar_storage_order ("big-endian")));
 #endif
 
 #if DSA_TAG_VERSION == 3
@@ -78,6 +91,48 @@ struct INR_DSA_TAG_RX		// to cpu
   uint64_t CTL_timestamp;
   uint32_t reserved3;
 
+
+} __attribute__((__packed__, scalar_storage_order ("big-endian")));
+struct INR_DSA_TAG_TX		//from CPU
+{
+
+  uint32_t DSA_ETH_TYPE:16;
+  uint32_t INGRESS_PORT:6;
+  uint32_t EGRESS_PORT:6;
+  uint32_t STREAM_Q:4;
+  uint32_t TX_TIMESTAMP;
+  uint32_t TX_CONFIRMATION_ID;
+  uint32_t reserved;
+
+} __attribute__((__packed__, scalar_storage_order ("big-endian")));
+#endif
+#if DSA_TAG_VERSION == 4
+#define INR_TAG_LEN	24
+struct INR_DSA_TAG_RX		// to cpu
+{
+
+  uint32_t DSA_ETH_TYPE:16;
+  uint32_t INGRESS_PORT:5;
+  uint32_t EGRESS_PORT:5;
+  uint32_t PROC_RSN:5;
+  uint32_t BAD_0:1;
+  uint32_t BRIDGE_timestamp;
+  uint32_t RX_timestamp;
+  uint64_t CTL_timestamp;
+  uint32_t reserved3;
+
+
+} __attribute__((__packed__, scalar_storage_order ("big-endian")));
+struct INR_DSA_TAG_TX		//from CPU
+{
+
+  uint32_t DSA_ETH_TYPE:16;
+  uint32_t INGRESS_PORT:5;
+  uint32_t EGRESS_PORT:5;
+  uint32_t STREAM_Q:6;
+  uint32_t TX_TIMESTAMP;
+  uint32_t TX_CONFIRMATION_ID;
+  uint32_t reserved;
 
 } __attribute__((__packed__, scalar_storage_order ("big-endian")));
 #endif
