9600142d18b5d83293504f5b06efb8fa1e51a04c
ulbricht-inr <ulbricht@innoroute.de>
2022-12-07 14:28:06 +0100

    Update tag_innoroute.h

--- net/dsa/tag_innoroute.h
+++ net/dsa/tag_innoroute.h
@@ -2,7 +2,7 @@
 *@file
 *@brief InnoRoute Realtime-Hat DSA-tag definitions
 *@author M.Ulbricht 2021
-*@copyright GNU Public License v3.
+*@copyright GNU Public License v2.
 *
 **/
 #define INR_DSA_ETH_TYPE 0x813E
